820a9f301f84f8ec76bf5920b56d46eb9b11eb3d
heyuhang0 <heyuhang0@outlook.com>
2020-06-20 23:25:47 +0800

    First commit of my installation script

---
+++ README.md
@@ -0,0 +1,14 @@
+# Yuhang's oh-my-zsh quick installer
+
+## Enabled plugins
+* zsh-syntax-highlighting
+* zsh-syntax-highlighting
+
+## Overridden default settings
+* DISABLE_AUTO_UPDATE="true"
+
+## Install
+
+```
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/heyuhang0/my-ohmyzsh/install.sh)"
+```
---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# install oh-my-zsh
+# see: https://github.com/ohmyzsh/ohmyzsh
+RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install zsh-autosuggestions
+# see: https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# insatll zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# apply template
+wget -O ~/.zshrc https://raw.githubusercontent.com/heyuhang0/hyh-ohmyzsh/master/zshrc.zsh-template
+sed -i "s/@TMP_USERNAME/$USER/g" ~/.zshrc
+
+# start zsh
+exec zsh -l
